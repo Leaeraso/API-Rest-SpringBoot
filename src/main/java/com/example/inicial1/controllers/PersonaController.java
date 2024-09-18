8a12c561fdcd870e1b438762255137a41e6f13e8
Leaeraso <leaeraso@gmail.com>
2024-09-18 20:02:36 -0300

    API Rest

---
+++ src/main/java/com/example/inicial1/controllers/PersonaController.java
@@ -0,0 +1,66 @@
+package com.example.inicial1.controllers;
+
+import com.example.inicial1.entities.Persona;
+import com.example.inicial1.services.PersonaService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+//  Indica que esta clase es un controlador en Spring que gestionará solicitudes HTTP y devolverá respuestas JSON.
+@RestController
+// Permite solicitudes desde cualquier origen.
+@CrossOrigin(origins = "*")
+// Define la ruta base para todas las solicitudes manejadas por este controlador.
+@RequestMapping(path = "api/v1/personas")
+public class PersonaController {
+    private PersonaService personaService;
+
+    public PersonaController(PersonaService personaService) {
+        this.personaService = personaService;
+    }
+
+    @GetMapping("")
+    public ResponseEntity<?> getAll() {
+        try {
+            return ResponseEntity.status(HttpStatus.OK).body(personaService.findAll());
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("{\"error\":\"Error. Por favor intente mas tarde.\"}");
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getOne(@PathVariable Long id) {
+        try {
+            return ResponseEntity.status(HttpStatus.OK).body(personaService.findById(id));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("{\"error\":\"Error. Por favor intente mas tarde.\"}");
+        }
+    }
+
+    @PostMapping("")
+    public ResponseEntity<?> save(@RequestBody Persona entity) {
+        try {
+            return ResponseEntity.status(HttpStatus.OK).body(personaService.save(entity));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("{\"error\":\"Error. Por favor intente mas tarde.\"}");
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> update(@PathVariable Long id,@RequestBody Persona entity) {
+        try {
+            return ResponseEntity.status(HttpStatus.OK).body(personaService.update(id, entity));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("{\"error\":\"Error. Por favor intente mas tarde.\"}");
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable Long id) {
+        try {
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).body(personaService.delete(id));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("{\"error\":\"Error. Por favor intente mas tarde.\"}");
+        }
+    }
+}
---
+++ src/main/java/com/example/inicial1/services/PersonaService.java
@@ -0,0 +1,81 @@
+package com.example.inicial1.services;
+
+import com.example.inicial1.entities.Persona;
+import com.example.inicial1.repositories.PersonaRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+// Indica que la clase es responsable de llevar a cabo operaciones complejas relacionadas con los datos(reglas de negocio).
+@Service
+public class PersonaService implements BaseService<Persona>{
+
+    private PersonaRepository personaRepository;
+
+    public PersonaService(PersonaRepository personaRepository){
+        this.personaRepository = personaRepository;
+    }
+
+    @Override
+    @Transactional
+    public List<Persona> findAll() throws Exception {
+        try {
+            List<Persona> entities = personaRepository.findAll();
+            return entities;
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
+        }
+    }
+
+    @Override
+    @Transactional
+    public Persona findById(Long id) throws Exception {
+        try {
+            Optional<Persona> entityOptional = personaRepository.findById(id);
+            return entityOptional.get();
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
+        }
+    }
+
+    @Override
+    @Transactional
+    public Persona save(Persona entity) throws Exception {
+        try {
+            entity = personaRepository.save(entity);
+            return entity;
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
+        }
+    }
+
+    @Override
+    @Transactional
+    public Persona update(Long id, Persona entity) throws Exception {
+        try {
+            Optional<Persona> entityOptional = personaRepository.findById(id);
+            Persona p = entityOptional.get();
+            p = personaRepository.save(entity);
+            return p;
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
+        }
+    }
+
+    @Override
+    @Transactional
+    public boolean delete(Long id) throws Exception {
+        try {
+            if(personaRepository.existsById(id)) {
+                personaRepository.deleteById(id);
+                return true;
+            } else {
+                throw new Exception();
+            }
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
+        }
+    }
+}
